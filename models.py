ee9b1c077105135c07eb4ebdc8886e2fac58d326
Ahmed Elhaddad <eehaddad@gmail.com>
2019-10-21 07:19:23 +0000

    Implement notes create and index

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from mistune import markdown
 
 db = SQLAlchemy()
 
@@ -20,4 +21,7 @@ class Note(db.Model):
     updated_at = db.Column(db.DateTime,
             server_default=db.func.now(), server_onupdate=db.func.now())
 
+    @property
+    def body_html(self):
+        return markdown(self.body)
 
